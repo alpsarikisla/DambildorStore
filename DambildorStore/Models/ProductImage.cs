f7d6b1810a78188e46560aaebdcf533dcc0389be
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-07-16 17:32:16 +0300

    Çoklu Resim Sistemi Eklendi

---
+++ DambildorStore/Models/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DambildorStore.Models
+{
+    public class ProductImage
+    {
+        public int ID { get; set; }
+
+        public int Product_ID { get; set; }
+
+        [ForeignKey("Product_ID")]
+        public virtual Product Product { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
