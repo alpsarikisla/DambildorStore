c914688b3a487c61aa4fe16562c88726b5e1c870
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-07-24 18:00:38 +0300

    Ödeme Sistemi Eklendi

--- DambildorStore/Controllers/UserCartController.cs
+++ DambildorStore/Controllers/UserCartController.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DambildorStore.Models;
+using DambildorStore.Models.ViewModels;
+using System.Net.Http;
+using Newtonsoft.Json;
+
 
 namespace DambildorStore.Controllers
 {
+    public class PayServiceModel
+    {
+        public string MerchantCode { get; set; }
+        public string MerchantPassword { get; set; }
+        public string CardNo { get; set; }
+        public string ExpM { get; set; }
+        public string ExpY { get; set; }
+        public string CVV { get; set; }
+        public decimal Price { get; set; }
+    }
     public class UserCartController : Controller
     {
         DambildorModel db = new DambildorModel();
@@ -79,6 +93,68 @@ namespace DambildorStore.Controllers
                 return View();
             }
             return RedirectToAction("Login", "User");
+        }
+
+        [HttpPost]
+        public ActionResult Buyproducts(PayViewModel model)
+        {
+            int id = ((User)Session["user"]).ID;
+            List<UserCart> userCartList = db.UserCarts.Where(x => x.User_ID == id).ToList();
+            decimal price = userCartList.Sum(x => x.Quantity * x.Product.Price);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("https://localhost:44375/API/PayService");
+                        //client.BaseAddress = new Uri("https://www.hortumbank.com.tr/API/PayService");
+                        PayServiceModel requestModel = new PayServiceModel()
+                        {
+                            MerchantCode = "12XYZ458",
+                            MerchantPassword = "14?*WE12",
+                            CardNo = model.CardNumber,
+                            ExpM = model.ExpM,
+                            ExpY = model.ExpY,
+                            CVV = model.Cvv,
+                            Price = price
+                        };
+                        //Install-Package Microsoft.AspNet.WebApi.Client
+                        var postTask = client.PostAsJsonAsync<PayServiceModel>("PayService", requestModel);
+                        postTask.Wait();
+                        var result = postTask.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            var stringResp = result.Content.ReadAsStringAsync();
+                            if (stringResp.Result == "\"101\"")
+                            {
+                                return RedirectToAction("PaySuccess", "UserCart");
+                            }
+                            else if(stringResp.Result == "\"401\"")
+                            {
+                                ViewBag.semitotal = userCartList.Sum(x => x.Quantity * x.Product.Price) * 0.82m;
+                                ViewBag.totalTax = userCartList.Sum(x => x.Quantity * x.Product.Price) * 0.18m;
+                                ViewBag.total = userCartList.Sum(x => x.Quantity * x.Product.Price);
+
+                                ViewBag.result = "Bakiye Yetersiz";
+                                return View();
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+           
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult PaySuccess()
+        {
+            return View();
         }
     }
 }
